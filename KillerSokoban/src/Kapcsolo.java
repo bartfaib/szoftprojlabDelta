38f40cba1367be07b52f05679106014c3be87a9c
Rybka3 <depprybka@gamil.com>
2018-03-17 20:47:29 +0100

    comm

---
+++ KillerSokoban/src/Kapcsolo.java
@@ -0,0 +1,25 @@
+
+public class Kapcsolo {
+	
+	private Lyuk lyuk;
+	
+	public Kapcsolo(){
+		System.out.println("Kapcsolo");
+	}
+	
+	public Kimenetel Mozog(Iranyok i){
+		
+	}
+	
+	public void Add(Munkas m){
+		
+	}
+	
+	public void Add(Lada l){
+		
+	}
+	
+	public void Torol(){
+		
+	}
+}
---
+++ KillerSokoban/src/Lyuk.java
@@ -0,0 +1,25 @@
+
+public class Lyuk {
+ private boolean Nyitott = false;
+ 
+ public Lyuk(){
+	 System.out.println("Lyuk");
+ }
+ 
+ public Kimenetel Mozog(Iranyok i){
+	 
+ }
+ 
+ public void Add(Munkas m){
+	 
+ }
+ 
+ public void Add(Lada l){
+	 
+ }
+ 
+ public void Torol(){
+	 
+ }
+ 
+}
