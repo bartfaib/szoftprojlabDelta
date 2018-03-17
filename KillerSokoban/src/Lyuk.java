a96f2a38bbd371c30c019a7774e87a776bd26f08
Rybka3 <depprybka@gamail.com>
2018-03-17 22:30:18 +0100

    javitott

--- KillerSokoban/src/Lyuk.java
+++ KillerSokoban/src/Lyuk.java
@@ -1,26 +1,32 @@
 
+//Lyuk osztaly
 public class Lyuk extends Mezo {
  private boolean Nyitott = false;
  
  public Lyuk(){
-	 System.out.println("Lyuk");
+	 System.out.println("Lyuk konstruktor");
  }
  
+ /*Megkerdezik hogy mozoghatnak e ra ennek megfeleloen vaaszol
+  * ha van rajta valami akkor annak is meghivja a mozog fuggvenyet
+  * @see Mezo#Mozog(Irany)
+  */
  public Kimenetel Mozog(Irany i){
 		Kimenetel a = null;
 		return a;
  }
- 
+ //Hozza ad egy munkast
  public void Add(Munkas m){
-	 
+	 System.out.println("Add (munkas)");
  }
- 
+ //Hozza ad egy ladat
  public void Add(Lada l){
-	 
+	 System.out.println("Add (lada)");
  }
- 
+ //Kitorli a rajta levo elemet
  public void Torol(){
-	 
+	 System.out.println("Torol");
  }
  
 }
+
